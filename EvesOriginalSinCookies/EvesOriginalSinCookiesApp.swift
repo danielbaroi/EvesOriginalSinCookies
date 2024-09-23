1a732ee4a6f9363131d8457394a8e77fd193b5db
Daniel Baroi <danielbaroi@cedarville.edu>
2024-09-23 14:18:25 -0400

    Initial Commit

---
+++ EvesOriginalSinCookies/EvesOriginalSinCookiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvesOriginalSinCookiesApp.swift
+//  EvesOriginalSinCookies
+//
+//  Created by Daniel Baroi on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct EvesOriginalSinCookiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
